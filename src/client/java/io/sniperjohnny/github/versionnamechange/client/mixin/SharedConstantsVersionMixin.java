7c5717386346c07e09a8e94bbe79245439efcd6f
SniperJohnny <johnjaekel2011@gmail.com>
2026-07-28 18:51:38 +0200

    version 26.2 finished

--- src/client/java/io/sniperjohnny/github/versionnamechange/client/mixin/SharedConstantsVersionMixin.java
+++ src/client/java/io/sniperjohnny/github/versionnamechange/client/mixin/SharedConstantsVersionMixin.java
@@ -27,12 +27,12 @@ public class SharedConstantsVersionMixin {
         cir.setReturnValue(new WorldVersion() {
             @Override
             public String id() {
-                return original.id().replace("26.1.2", "1.22.2");
+                return original.id().replace("26.2", "1.22.3");
             }
 
             @Override
             public String name() {
-                return original.name().replace("26.1.2", "1.22.2");
+                return original.name().replace("26.2", "1.22.3");
             }
 
             @Override
